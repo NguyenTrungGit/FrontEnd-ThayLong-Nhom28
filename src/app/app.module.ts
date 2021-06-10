2770dead5d9ab10779177818fa0bf70e9b50f23a
BaoTruong1311 <76053492+BaoTruong1311@users.noreply.github.com>
2021-06-10 21:25:05 +0700

    Trang danh sach san pham

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,24 @@ import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './NTrung/product-details/product-details.component';
 import { HeaderComponent } from './Tuyen/header/header.component';
 import { FooterComponent } from './Tuyen/footer/footer.component';
+import { ProductComponent } from './BTruong/product/product.component';
+import { ProductListComponent } from './BTruong/product/product-list/product-list.component';
+import { PagingComponent } from './BTruong/product/paging/paging.component';
+import { FiltersComponent } from './BTruong/product/filters/filters.component';
+import { ProductItemComponent } from './BTruong/product/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProductComponent,
+    ProductListComponent,
+    PagingComponent,
+    FiltersComponent,
+    HeaderComponent,
+    ProductItemComponent,
   ],
   imports: [
     BrowserModule,
