25e29d26964422f7c5c57157b030a0701f02c551
tuyen-cpu-i5 <18130277@st.hcmuaf.edu.vn>
2021-06-22 00:22:10 +0700

    tao dich vu san pham cho trang web

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,12 @@
+export class Product{
+  id?: number;
+  name?: string;
+  price?: number;
+  quantity?: number;
+  category?: string;
+  origin?: string;
+  img1?: string;
+  img2?: string;
+  img3?: string;
+  description?: string;
+}
