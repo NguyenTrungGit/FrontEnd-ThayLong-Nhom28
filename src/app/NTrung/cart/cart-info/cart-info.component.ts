5b5ce112ebc1e4b330b8bfd04defc27a7725aef8
NguyenTrungGit <18130258@st.hcmuaf.edu.vn>
2021-07-01 12:53:26 +0700

    NTrung/Create some method

--- src/app/NTrung/cart/cart-info/cart-info.component.ts
+++ src/app/NTrung/cart/cart-info/cart-info.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-info',
   templateUrl: './cart-info.component.html',
-  styleUrls: ['./cart-info.component.css']
+  styleUrls: ['./cart-info.component.css'],
 })
 export class CartInfoComponent implements OnInit {
+  changeValue?: String;
+  changeNum?: number =1;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getValue(val: String) {
+    // ham nay cho phep lay gia tri khi nhan phim
+    // tao mot bien la changeValue, gan bien bang gia tri ham thay doi, bindling du lieu nguoc lai se thay doi du lieu
+    console.warn(val);
+    this.changeValue = val;
   }
-
 }
