6dcd52d7f0b81cbc4a750af1aaeaed686c89a076
NguyenTrungGit <18130258@st.hcmuaf.edu.vn>
2021-08-07 17:48:41 +0700

    NTrung/Updated Product detail UI

---
+++ src/app/NTrung/product-detail/contact-evaluate/contact-evaluate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-evaluate',
+  templateUrl: './contact-evaluate.component.html',
+  styleUrls: ['./contact-evaluate.component.css']
+})
+export class ContactEvaluateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
