2513d28ae2ee0ef4b165e4984e1b4ec616a9260a
NguyenTrungGit <18130258@st.hcmuaf.edu.vn>
2021-08-07 09:35:38 +0700

    NTrung/Updated bill confirm UI

---
+++ src/app/NTrung/bill-confirm/bill-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-confirm',
+  templateUrl: './bill-confirm.component.html',
+  styleUrls: ['./bill-confirm.component.css']
+})
+export class BillConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
