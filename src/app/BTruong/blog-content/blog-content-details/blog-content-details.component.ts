03a87903f48127ad31ce7af24e9bbe7ba71b61c0
BaoTruong1311 <76053492+BaoTruong1311@users.noreply.github.com>
2021-08-20 00:12:39 +0700

    Reponsive search. Fix search. Fix reponsive contact, blog and product. Validation form

--- src/app/BTruong/blog-content/blog-content-details/blog-content-details.component.ts
+++ src/app/BTruong/blog-content/blog-content-details/blog-content-details.component.ts
@@ -2,6 +2,10 @@ import { Component, Input, OnInit,SimpleChange,SimpleChanges } from '@angular/co
 import { Blog } from 'src/app/model/blog.model';
 import { Product } from 'src/app/model/product.model';
 import { ProductService } from 'src/app/Services/product.service';
+import { Comment } from 'src/app/model/comment.model';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { CommentService } from 'src/app/Services/NTrung/comment.service';
+
 
 @Component({
   selector: 'app-blog-content-details',
@@ -14,7 +18,8 @@ export class BlogContentDetailsComponent implements OnInit {
  id:any;
  blogInfor?:Blog
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private _fb: FormBuilder,
+    public commentService: CommentService) { }
 
   ngOnInit(): void {
     this.id= this.idInput;
@@ -31,7 +36,33 @@ export class BlogContentDetailsComponent implements OnInit {
       this.blogInfor = res.find((n) => n.id === this.id);
     });
   }
-
-
-
+  onFormSubmit(form: any) {
+    const comment = new Comment(
+      form.value.fristName,
+      form.value.email,
+      form.value.phoneNum,
+      form.value.content,
+      form.value.rating
+    );
+    this.commentService.addComment(comment);
+    this.evaluateGroup.reset();
+  }
+  evaluateGroup: FormGroup = this._fb.group({
+    fristName: [null, [Validators.required, Validators.minLength(2)]],
+    phoneNum: [
+      null,
+      [Validators.required, Validators.pattern('^((\\+84-?)|0)?[0-9]{10}$')],
+    ],
+    email: [
+      null,
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ),
+      ],
+    ],
+    content: [null, Validators.required],
+    rating: [null],
+  });
 }
