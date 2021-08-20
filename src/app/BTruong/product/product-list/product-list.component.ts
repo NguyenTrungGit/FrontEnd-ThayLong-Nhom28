62c536d4a01c2a31db3525e95e4c166a0d6b110f
NguyenTrungGit <18130258@st.hcmuaf.edu.vn>
2021-08-20 12:19:10 +0700

    NTrung/Fix url pagination

--- src/app/BTruong/product/product-list/product-list.component.ts
+++ src/app/BTruong/product/product-list/product-list.component.ts
@@ -22,6 +22,7 @@ export class ProductListComponent implements OnInit {
   total_count: any;
   activePage: any = 1;
   pager: any = {};
+
   constructor(
     private productService: ProductService,
     private activatedRoute: ActivatedRoute,
@@ -29,118 +30,124 @@ export class ProductListComponent implements OnInit {
   ) {}
   ngOnChanges(changes: SimpleChanges): void {
     this.categoryTemp = this.categoryName;
-    this.pager ={}
+    this.pager = {};
     this.getProducts();
   }
 
   ngOnInit(): void {
     this.categoryTemp = this.categoryName;
-    this.activatedRoute.queryParams.subscribe(params => {
-     this.minPrice=params['min']
-      this.maxPrice=params['max']
-      this.activePage=params['trang']
-if(this.activePage===undefined){
-  console.log("para trang ko có")
-this.activePage=1
-}else{
-  console.log("para: "+this.activePage)
-}
-
-  });
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.minPrice = params['min'];
+      this.maxPrice = params['max'];
+      this.activePage = params['trang'];
+      if (this.activePage === undefined) {
+        // console.log('para trang ko có');
+        this.activePage = 1;
+      } else {
+        // console.log('para: ' + this.activePage);
+      }
+    });
     this.getProducts();
   }
-  getProducts(){
-    console.log(this.minPrice+"_"+this.maxPrice)
-    if(this.minPrice===undefined&&this.maxPrice===undefined){
+
+  getProducts() {
+    if (this.minPrice === undefined && this.maxPrice === undefined) {
       this.getProductWithoutParaPrice();
-    }else{
+    } else {
       this.getProductWithParaPrice();
     }
-    console.log(this.datas)
-
   }
-  getProductWithoutParaPrice(){
+
+  // get product with category (no price)
+  getProductWithoutParaPrice() {
     if (this.categoryName === 'tatca') {
+      // get length of list product all
       this.productService.getProducts().subscribe((res: any) => {
-          this.total_count = res.length;
+        this.total_count = res.length;
+        this.pager = this.productService.getPager(
+          this.total_count,
+          this.activePage
+        );
+      });
+
+      this.productService
+        .getProductsNomal(this.activePage)
+        .subscribe((res: any) => {
+          this.datas = res;
         });
-      this.productService.getProductsNomal(this.activePage).subscribe((res: any) => {
-            this.datas = res;
-            this.pager = this.productService.getPager(
+    } else {
+      // get length of list product (category)
+      this.productService
+        .getProductsByCategory(this.categoryTemp)
+        .subscribe((res: any) => {
+          this.total_count = res.length;
+          this.pager = this.productService.getPager(
             this.total_count,
             this.activePage
           );
         });
-    } else {
+
       this.productService
-        .getProductsByCategory(
-          this.categoryTemp
-        )
+        .getProductsByCategoryNomal(this.categoryTemp, this.activePage)
+        .subscribe((res: any) => {
+          this.datas = res;
+        });
+    }
+  }
+
+  // get product with category and price
+  getProductWithParaPrice() {
+    if (this.categoryName === 'tatca') {
+      // console.log('all+ có price' + this.maxPrice);
+      this.productService
+        .getLengthByPrice(this.minPrice || 0, this.maxPrice || 0)
         .subscribe((res: any) => {
           this.total_count = res.length;
+          this.pager = this.productService.getPager(
+            this.total_count,
+            this.activePage
+          );
         });
       this.productService
-        .getProductsByCategoryNomal(
-          this.categoryTemp,
+        .getProductsByPrice(
+          this.minPrice || 0,
+          this.maxPrice || 0,
           this.activePage
         )
         .subscribe((res: any) => {
           this.datas = res;
-          this.pager = this.productService.getPager(
-            this.total_count,
-            this.activePage
-          );
-        });
-
-    }
-  }
-  getProductWithParaPrice(){
-    if (this.categoryName ==='tatca') {
-      console.log("all+ có price"+this.maxPrice)
-      this.productService
-        .getLengthByPrice(this.minPrice||0, this.maxPrice||0)
-        .subscribe((res: any) => {
-          this.total_count = res.length;
-        });
-      this.productService
-        .getProductsByPrice(this.minPrice||0, this.maxPrice||0, this.activePage)
-        .subscribe((res: any) => {
-          this.datas = res;
-          this.pager = this.productService.getPager(
-            this.total_count,
-            this.activePage
-          );
         });
       //  this.router.navigate([], {  queryParams: {  min:this.minPrice,max:this.maxPrice} });
     } else {
       this.productService
         .getLengthByPriceAndCategory(
-          this.minPrice||0,
-          this.maxPrice||0,
+          this.minPrice || 0,
+          this.maxPrice || 0,
           this.categoryTemp
         )
         .subscribe((res: any) => {
           this.total_count = res.length;
-        });
-      this.productService
-        .getProductsByPriceAndCategory(
-          this.minPrice||0,
-          this.maxPrice||0,
-          this.categoryTemp,
-          this.activePage
-        )
-        .subscribe((res: any) => {
-          this.datas = res;
+          // console.log(this.activePage)
           this.pager = this.productService.getPager(
             this.total_count,
             this.activePage
           );
         });
+      this.productService
+        .getProductsByPriceAndCategory(
+          this.minPrice || 0,
+          this.maxPrice || 0,
+          this.categoryTemp,
+          this.activePage
+        )
+        .subscribe((res: any) => {
+          this.datas = res;
+        });
 
-
-       // this.router.navigate([], {  queryParams: {  min:this.minPrice,max:this.maxPrice} });
+      // this.router.navigate([], {  queryParams: {  min:this.minPrice,max:this.maxPrice} });
     }
   }
+
   sort(event: any) {
     switch (event.target.value) {
       case '': {
@@ -192,27 +199,6 @@ this.activePage=1
     return this.datas;
   }
 
-  setPage(page: number) {
-    this.scrollOnTop();
-    if (this.maxPrice !== 99999999 && this.minPrice !== 0) {
-      this.router.navigate([], {
-        queryParams: { min: this.minPrice, max: this.maxPrice, trang: page },
-      });
-    } else {
-      this.router.navigate([], { queryParams: { trang: page } });
-    }
-
-    if (page < 1 || page > this.pager.totalPages) {
-      return;
-    }
-    this.activePage = page;
-    this.pager = this.productService.getPager(
-      this.total_count,
-      this.activePage
-    );
-    this.getProducts();
-  }
-
   scrollOnTop() {
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
@@ -221,8 +207,33 @@ this.activePage=1
       window.scroll({
         top: 0,
         left: 0,
-        behavior: 'smooth'
-       });
+        behavior: 'smooth',
+      });
     });
+  }
+
+  setPage(page: number) {
+    this.scrollOnTop();
+    // send param to url
+    if (this.maxPrice !== 99999999 && this.minPrice !== 0) {
+      this.router.navigate([], {
+        queryParams: { min: this.minPrice, max: this.maxPrice, trang: page },
+      });
+    } else {
+      this.router.navigate([], { queryParams: { trang: page } });
+    }
+
+    // setPager
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    this.activePage = page;
+    this.pager = this.productService.getPager(
+      this.total_count,
+      this.activePage
+    );
+
+    this.getProducts();
   }
 }
